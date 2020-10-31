913c7c256b6c4f5a1de31830b18268dece561292
bona <bona373737@gmail.com>
2020-10-31 12:30:53 +0900

    학생목록을 뿌려주는 어탭터 기초 작업

---
+++ app/src/main/java/adapters/adapters.kt
@@ -0,0 +1,29 @@
+package adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.t.listview_20201031.R
+import com.t.listview_20201031.student
+import java.math.MathContext
+
+class adapters (val mathContext: Context,
+                val resId: Int,
+                val mList: ArrayList<student>): ArrayAdapter<student>(mathContext,resId,mList) {
+
+    val inf = LayoutInflater.from(mathContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null){
+            tempRow = inf.inflate(R.layout.student_list_item,null)
+        }
+        val row = tempRow!!
+
+        return row
+    }
+
+
+}
